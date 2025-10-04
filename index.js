4137abcbbbe7e9a0f0e54fa5ea24af7e5d5ab1a9
mhmasum1 <mhmasum231@gmail.com>
2025-10-04 17:48:24 +0600

    added app post coffees

--- index.js
+++ index.js
@@ -20,8 +20,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const coffeesCollecction = client.db('coffeeDB').collection('coffees')
+
+        app.post('/coffees', async (req, res) => {
+            const newCoffe = req.body;
+            console.log(newCoffe);
+            const result = await coffeesCollecction.insertOne(newCoffe);
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
